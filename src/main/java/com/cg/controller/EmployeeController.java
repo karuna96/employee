2d99cca9f7ae6ca71f06d6ba43f038fd2f6c547a
CHERUPALLY <karuna.cherupally@capgemini.com>
2019-05-02 09:51:48 +0530

    final commit

--- src/main/java/com/cg/controller/EmployeeController.java
+++ src/main/java/com/cg/controller/EmployeeController.java
@@ -3,9 +3,11 @@ package com.cg.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -59,5 +61,20 @@ public class EmployeeController {
 	public employeeEntity getById(@PathVariable Long id)
 	{
 		return employeeRepository.getOne(id);
+	}
+	
+	@PutMapping("/update/{id}/{name}")
+	public employeeEntity updateById(@PathVariable Long id, @PathVariable String name) {
+		employeeEntity employee=employeeRepository.getOne(id);
+		employee.setName(name);
+		employeeRepository.saveAndFlush(employee);
+		return employee;
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public employeeEntity deleteById(@PathVariable Long id) {
+		employeeEntity employee=employeeRepository.getOne(id);
+		employeeRepository.deleteById(id);
+		return employee;
 	}
 }
